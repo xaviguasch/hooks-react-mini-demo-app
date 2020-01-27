190e1a7f4597c26864e0ab5f003a4a5cce53b913
xaviguasch <xaviguasch@gmail.com>
2020-01-27 10:09:57 +0100

    added password input

--- src/SimpleFormInputHook.js
+++ src/SimpleFormInputHook.js
@@ -3,14 +3,21 @@ import useInputState from './hooks/useInputState'
 
 const SimpleFormInputHook = () => {
   const [email, updateEmail, resetEmail] = useInputState('')
+  const [password, updatePassword, resetPassword] = useInputState('')
 
   return (
     <div className='container text-center'>
       <h1 className='text-danger'>SimpleFormInputHook</h1>
-      <h1>The value is... {email}</h1>
+      <h1>
+        The value is {email} and password is: {password}
+      </h1>
       <input type='text' value={email} onChange={updateEmail} />
       <button className='btn btn-success' onClick={resetEmail}>
         Submit
+      </button>
+      <input type='password' value={password} onChange={updatePassword} />
+      <button className='btn btn-info' onClick={resetPassword}>
+        Reset
       </button>
     </div>
   )
